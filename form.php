411d0cb70af76e26828fe63d7ca32dd62ccd36f1
alvarosca13 <scarafiaalvaro99@gmail.com>
2022-05-17 11:56:28 -0300

    Corrigiendo form

--- form.php
+++ form.php
@@ -6,18 +6,18 @@ $numero = $_POST['number'];
 $mensaje = $_POST['consulta'];
 
 
-$mensaje = "Este mensaje fue enviado por " , $nombre, ",/r/n";
-$mensaje ,= "Su email es : " , $email , ",/r/n";
-$mensaje ,= "Numero de Telefono" , $numero , ",/r/n";
-$mensaje ,= "Mensaje:" , $_POST['mensaje'] , ",/r/n" ;
-$mensaje ,= "Enviado el " , date('d/m/Y', time());
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",/r/n";
+$mensaje .= "Su email es : " . $email . ",/r/n";
+$mensaje .= "Numero de Telefono" . $numero . ",/r/n";
+$mensaje .= "Mensaje:" . $_POST['mensaje'] . ",/r/n" ;
+$mensaje .= "Enviado el " . date('d/m/Y'. time());
 
 $destinatario = 'alvaro_s.d@hotmail.com';
 $asunto = 'Este mail fue enviado desde la web SOLANO DISEÑO';
 
 
 
-mail($destinatario, $asunto, utf8_decode($mensaje), $header);
+mail($destinatario, $asunto, utf8_decode($nombre, $email, $mensaje), $header);
 
 header('Location:exito.html');
 
