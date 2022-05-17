c1c4231a87ca0da6531dc40c885f57c9b1172f32
alvarosca13 <scarafiaalvaro99@gmail.com>
2022-05-17 11:53:26 -0300

    Terminando Formulario

---
+++ form.php
@@ -0,0 +1,28 @@
+<?php
+
+$nombre = $_POST['full-name'];
+$email = $_POST['email'];
+$numero = $_POST['number'];
+$mensaje = $_POST['consulta'];
+
+
+$mensaje = "Este mensaje fue enviado por " , $nombre, ",/r/n";
+$mensaje ,= "Su email es : " , $email , ",/r/n";
+$mensaje ,= "Numero de Telefono" , $numero , ",/r/n";
+$mensaje ,= "Mensaje:" , $_POST['mensaje'] , ",/r/n" ;
+$mensaje ,= "Enviado el " , date('d/m/Y', time());
+
+$destinatario = 'alvaro_s.d@hotmail.com';
+$asunto = 'Este mail fue enviado desde la web SOLANO DISEÑO';
+
+
+
+mail($destinatario, $asunto, utf8_decode($mensaje), $header);
+
+header('Location:exito.html');
+
+
+
+
+
+?>
