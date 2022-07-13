e2cd5b5dcfeb0bd540eb693e28edadf50e9b445a
Alvaro Scarafia <alvaro_s.d@hotmail.com>
2022-07-13 16:22:11 -0300

    Corrigiendo DOM y HTML del carrito

--- js/carrito.js
+++ js/carrito.js
@@ -5,13 +5,15 @@ agregarAlCarritoButtons.forEach(agregarAlCarritoBoton => {
 
 const containerProductosCarrito = document.querySelector(".containerProductosCarrito");
 
+                                                        /* FUNCION PARA AGREGAR AL CARRITO */
+
 function clickParaAgregar(event){
     const boton = event.target;
     const item = boton.closest(".card");
     
     const nombreProductoCarrito = item.querySelector(".card-title").textContent;
     const precioProductoCarrito = item.querySelector(".precioProducto").textContent;
-    const imagenProductoCarrito = item.querySelector(".imgProducto").src
+    const imagenProductoCarrito = item.querySelector(".imgProducto").src;
 
     agregarProductosAlCarrito(nombreProductoCarrito,precioProductoCarrito,imagenProductoCarrito);
 }
@@ -19,27 +21,47 @@ function clickParaAgregar(event){
 function agregarProductosAlCarrito (nombreProductoCarrito,precioProductoCarrito,imagenProductoCarrito){
     const seccionCarrito = document.createElement('div');
 
-    const productoEnCarrito = `<div class="row shoppingCartItem">
+    const productoEnCarrito = `
+<div class="row productoEnCarrito">
     <div class="col-6">
-        <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-            <img src=${imagenProductoCarrito} class="shopping-cart-image">
-            <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${nombreProductoCarrito}</h6>
+        <div class="productoEnCarritoCss d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+            <img src=${imagenProductoCarrito} class="imagenProductoCarrito">
+            <h6 class="tituloProductoCarritoCss tituloProductoCarritoJs text-truncate ml-3 mb-0">${nombreProductoCarrito}</h6>
         </div>
     </div>
     <div class="col-2">
-        <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-            <p class="item-price mb-0 shoppingCartItemPrice width="20px" ">${precioProductoCarrito}</p>
+        <div class="precioEnCarrito d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+            <p class="item-price mb-0 precioEnCarritoJs width="20px" ">${precioProductoCarrito}</p>
         </div>
     </div>
     <div class="col-4">
-        <div
-            class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
-            <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
-                value="1">
+        <div class="cantidadProdCarrito d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
+            <input class="cantidadProdCarritoJs" type="number" value="1">
             <button class="btn btn-danger buttonDelete" type="button">X</button>
         </div>
     </div>
 </div> `;
+
 seccionCarrito.innerHTML = productoEnCarrito;
 containerProductosCarrito.append(seccionCarrito);
+
+actualizacionPrecioCarrito();
+}
+
+                                                        /* ACTUALIZACION DE PRECIO EN CARRITO */
+
+function actualizacionPrecioCarrito(){
+    let total = 0;
+    const totalCarrito = document.querySelector(".totalCarritoJs");
+    
+    const productosEnCarrito = document.querySelectorAll(".productoEnCarrito");
+    
+    productosEnCarrito.forEach((productoEnCarrito) => {
+        const elementoCarritoPrecio = productoEnCarrito.querySelector(".precioEnCarritoJs");
+        const carritoPrecio = Number(elementoCarritoPrecio.textContent.replace("$", ""));
+        
+        const cantidadProductoCarritoElement = productoEnCarrito.querySelector(".cantidadProdCarritoJs");
+        const cantidadProductoCarrito = cantidadProductoCarritoElement.value;
+        console.log(cantidadProductoCarrito);
+    })
 }
